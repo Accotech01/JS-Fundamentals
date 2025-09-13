b3375b8bf3e1f31649d5e8d5c1b21b88a87cbcd7
accotech01 <accotech01@gmail.com>
2025-09-13 10:46:01 +0100

    added 8-square.js file

---
+++ 8-square.js
@@ -0,0 +1,20 @@
+let args = process.argv.slice(2)
+
+let arg = Number(args[0]);
+
+
+if (isNaN(arg)){
+    console.log("Missing size")
+
+} else {
+
+    for (let i = 0; i<arg; i++){
+
+        let row =' ';
+        for(let j = 0; j<arg; j++){
+            row += 'X '
+    }
+    console.log(row)
+}
+
+}
